f51a78a960a90759939157720d934f988506f115
NarendarTrainer <narendartrainer@gmail.com>
2025-07-28 09:51:44 +0530

    Stage-3(9:51am)

--- books/urls.py
+++ books/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home
+from .views import BooksListView
 urlpatterns=[
-    path("",home,name="home"),
+    path("",BooksListView.as_view(),name="list"),
 ]
--- books/views.py
+++ books/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Book
 
-def home(request):
-    return render(request,"base.html")
+
+class BooksListView(ListView):
+    model=Book
+    template_name='list.html'
